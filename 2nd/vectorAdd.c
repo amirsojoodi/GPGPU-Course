ec5a9e03b691243d5a6adfb08af2677402ea465d
Amir Hossein Sojoodi <amir.sojoodi@gmail.com>
2018-10-06 18:18:28 +0330

    Add 3rd session codes. Describe space locality

--- 2nd/vectorAdd.c
+++ 2nd/vectorAdd.c
@@ -3,148 +3,144 @@
 #include<sys/time.h>
 #include<time.h>
 
-typedef struct node{
-	int a;
-	int b[100];
-} node;
-
-void sequential_naive_vectorAdd(node *first, node *second, node *out, int size){
+void sequential_naive_vectorAdd(int *a, int *b, int *c, int size){
 	for(int i = 0; i < size; i++){
-		out[i].a = first[i].a + second[i].a;
+		c[i] = a[i] + b[i];
 	}
 }
 
-void sequential_vectorAdd_first_optimization(node *first, node *second, node *out, int size){
-    for(int i = 0; i < size / 2; i++){
-        out[i].a = first[i].a + second[i].a;
-        out[size - i - 1].a = first[size - i - 1].a + second[size - i - 1].a;
-    }
-}
-
-void sequential_vectorAdd_second_optimization(node *first, node *second, node *out, int size){
-    for(int i = 0; i < size; i += 2){
-        out[i].a = first[i].a + second[i].a;
-        out[i+1].a = first[i+1].a + second[i+1].a;
-    }
-}
-
-void sequential_vectorAdd_third_optimization(node *first, node *second, node *out, int size){
-    for(int i = 0, j = size / 2; i < size / 2; i++, j++){
-        out[i].a = first[i].a + second[i].a;
-        out[j].a = first[j].a + second[j].a;
-    }
-}
-
-void sequential_vectorAdd_fourth_optimization(node *first, node *second, node *out, int size){
-    for(int i = 0; i < size; i += 8){
-        out[i].a = first[i].a + second[i].a;
-        out[i+1].a = first[i+1].a + second[i+1].a;
-        out[i+2].a = first[i+2].a + second[i+2].a;
-        out[i+3].a = first[i+3].a + second[i+3].a;
-        out[i+4].a = first[i+4].a + second[i+4].a;
-        out[i+5].a = first[i+5].a + second[i+5].a;
-        out[i+6].a = first[i+6].a + second[i+6].a;
-        out[i+7].a = first[i+7].a + second[i+7].a;
-    }
+void sequential_vectorAdd_first_optimization(int *a, int *b, int *c, int size){
+	for(int i = 0; i < size / 2; i++){
+		c[i] = a[i] + b[i];
+		c[size - i - 1] = a[size - i - 1] + b[size - i - 1];
+	}
 }
 
 
-void print_array(char *array_name, node *array, int size){
+void sequential_vectorAdd_second_optimization(int *a, int *b, int *c, int size){
+	for(int i = 0; i < size; i += 2){
+        c[i] = a[i] + b[i];
+        c[i+1] = a[i+1] + b[i+1];
+    }   
+}
+
+void sequential_vectorAdd_third_optimization(int *a, int *b, int *c, int size){
+	for(int i = 0, j = size / 2; i < size / 2; i++, j++){
+        c[i] = a[i] + b[i];
+        c[j] = a[j] + b[j];
+    }   
+}
+
+void sequential_vectorAdd_forth_optimization(int *a, int *b, int *c, int size){
+	for(int i = 0; i < size; i += 8){
+        c[i] = a[i] + b[i];
+        c[i+1] = a[i+1] + b[i+1];
+        c[i+2] = a[i+2] + b[i+2];
+        c[i+3] = a[i+3] + b[i+3];
+        c[i+4] = a[i+4] + b[i+4];
+        c[i+5] = a[i+5] + b[i+5];
+        c[i+6] = a[i+6] + b[i+6];
+        c[i+7] = a[i+7] + b[i+7];
+    }   
+}
+
+void print_array(char *array_name, int *array, int size){
 	printf("\n%s: \n[", array_name);
 	for(int i = 0; i < size; i++){
-		printf("%d, ", array[i].a);
+		printf("%d, ", array[i]);
 	}
 	printf("]\n");
 }
 
 int main(int argc, char *argv[]){
 
-	node *first, *second, *out;
+	int *a, *b, *c;
 	struct timeval start, end;
-	time_t t;
 
 	int size = atoi(argv[1]);
 
-	first = (node *)malloc(sizeof(node) * size);
-	second = (node *)malloc(sizeof(node) * size);
-	out = (node *)malloc(sizeof(node) * size);
-
-	srand((unsigned) time(&t));
+	a = (int *)malloc(sizeof(int) * size);
+	b = (int *)malloc(sizeof(int) * size);
+	c = (int *)malloc(sizeof(int) * size);
 
 	for(int i=0; i < size; i++){
-		first[i].a = rand() % 100;
-		second[i].a = rand() % 100;
-		out[i].a = 0;
+		a[i] = rand() % 100;
+		b[i] = rand() % 100;
+		c[i] = 0;
 	}
 
 	// naive
 
-    gettimeofday(&start, NULL);
+	gettimeofday(&start, NULL); 
 
-    sequential_naive_vectorAdd(first, second, out, size);
+	sequential_naive_vectorAdd(a, b, c, size);
 
-    gettimeofday(&end, NULL);
-
-    double diff = (end.tv_sec - start.tv_sec) * 1000000.0 +
-        (end.tv_usec - start.tv_usec);
-
-    printf("Naive VectorAdd time calculation duration: %8.5fms\n", diff / 1000);
-
-    // first
-    gettimeofday(&start, NULL);
-
-    sequential_vectorAdd_first_optimization(first, second, out, size);
-
-    gettimeofday(&end, NULL);
-
-	diff = (end.tv_sec - start.tv_sec) * 1000000.0 +
-        (end.tv_usec - start.tv_usec);
-
-    printf("First VectorAdd time calculation duration: %8.5fms\n", diff / 1000);
-
-    // two
-    gettimeofday(&start, NULL);
-
-    sequential_vectorAdd_second_optimization(first, second, out, size);
-
-    gettimeofday(&end, NULL);
-
-    diff = (end.tv_sec - start.tv_sec) * 1000000.0 +
-        (end.tv_usec - start.tv_usec);
-
-    printf("Second VectorAdd time calculation duration: %8.5fms\n", diff / 1000);
-
-    // three
-    gettimeofday(&start, NULL);
-
-    sequential_vectorAdd_third_optimization(first, second, out, size);
-
-    gettimeofday(&end, NULL);
-
-    diff = (end.tv_sec - start.tv_sec) * 1000000.0 +
-        (end.tv_usec - start.tv_usec);
-
-	printf("Third VectorAdd time calculation duration: %8.5fms\n", diff / 1000);
-
-    // four
-    gettimeofday(&start, NULL);
-
-    sequential_vectorAdd_fourth_optimization(first, second, out, size);
-
-    gettimeofday(&end, NULL);
-
-    diff = (end.tv_sec - start.tv_sec) * 1000000.0 +
-        (end.tv_usec - start.tv_usec);
-
-    printf("Fourth VectorAdd time calculation duration: %8.5fms\n", diff / 1000);
+	gettimeofday(&end, NULL); 
 	
+	double diff = (end.tv_sec - start.tv_sec) * 1000000.0 + 
+		(end.tv_usec - start.tv_usec);
+	
+	printf("Naive VectorAdd time calculation duration: %8.5fms\n", diff / 1000);
+	
+	// first
+	gettimeofday(&start, NULL); 
+
+	sequential_vectorAdd_first_optimization(a, b, c, size);
+
+	gettimeofday(&end, NULL); 
+	
+	diff = (end.tv_sec - start.tv_sec) * 1000000.0 + 
+		(end.tv_usec - start.tv_usec);
+	
+	printf("First VectorAdd time calculation duration: %8.5fms\n", diff / 1000);
+	
+	// two
+	gettimeofday(&start, NULL); 
+
+	sequential_vectorAdd_second_optimization(a, b, c, size);
+
+	gettimeofday(&end, NULL); 
+	
+	diff = (end.tv_sec - start.tv_sec) * 1000000.0 + 
+		(end.tv_usec - start.tv_usec);
+	
+	printf("Second VectorAdd time calculation duration: %8.5fms\n", diff / 1000);
+	
+	// three
+	gettimeofday(&start, NULL); 
+
+	sequential_vectorAdd_third_optimization(a, b, c, size);
+
+	gettimeofday(&end, NULL); 
+	
+	diff = (end.tv_sec - start.tv_sec) * 1000000.0 + 
+		(end.tv_usec - start.tv_usec);
+	
+	printf("Third VectorAdd time calculation duration: %8.5fms\n", diff / 1000);
+	
+	// four
+	gettimeofday(&start, NULL); 
+
+	sequential_vectorAdd_forth_optimization(a, b, c, size);
+
+	gettimeofday(&end, NULL); 
+	
+	diff = (end.tv_sec - start.tv_sec) * 1000000.0 + 
+		(end.tv_usec - start.tv_usec);
+	
+	printf("Fourth VectorAdd time calculation duration: %8.5fms\n", diff / 1000);
+	// TODO: validation
+	// TODO: validation
+	// TODO: validation
+	// TODO: validation
+	// TODO: validation
 	//print_array("A", a, size);
 	//print_array("B", b, size);
 	//print_array("C", c, size);
 
-	free(first);
-	free(second);
-	free(out);
+	free(a);
+	free(b);
+	free(c);
 
 	return 0;
 }
