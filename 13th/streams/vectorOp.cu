3210566a61caaf77eed8866ad50739ae1104c5a1
Amir Hossein Sojoodi <amir.sojoodi@gmail.com>
2018-12-24 16:09:09 +0330

    Update 13th session's coded: streams

--- 13th/streams/vectorOp.cu
+++ 13th/streams/vectorOp.cu
@@ -1,16 +1,10 @@
 #include"vectorOp.h"
 
-__global__ void vector_operation_kernel(int *output, int *data, int size, int work_per_thread){
+__global__ void vector_operation_kernel(int *output, int *data, int size){
 	
 	int tid = threadIdx.x + blockIdx.x * blockDim.x;
 
-	int si = tid * work_per_thread;
-	int ei = si + work_per_thread;
-	if (ei > size){
-		ei = size;
-	}
-	
-	for(tid = si; tid < ei; tid++){
+	if (tid < size){
 		output[tid] = OPERATION(data[tid]);
 	}
 }
@@ -30,23 +24,33 @@ int main(int argc, char *argv[]){
 	int data_size;
 	int *data_h, *output_h, *device_output_h;
 	int *data_d, *output_d;
-	int work_per_thread;
-	
+	int stream_count;
+	// int work_per_thread;
+
 	if(argc != 4){
 		printf("Correct way to execute this program is:\n");
-		printf("./vectorOp data_size block_size work_per_thread\n");
+		printf("./vectorOp data_size block_size stream_count\n");
 		printf("For example:\n./vectorOp 10000 512 4\n");
 		return 1;
 	}
 
 	data_size = atoi(argv[1]);
 	block_size = atoi(argv[2]);
-	work_per_thread = atoi(argv[3]);
+	stream_count = atoi(argv[3]);
+	// work_per_thread = atoi(argv[4]);
+
+	cudaStream_t streams[stream_count];
+
+	for(int i = 0; i < stream_count; i++){
+		cudaStreamCreate(&streams[i]);
+	}
 
 	// Initialize data on Host
-	initialize_data_random(&data_h, data_size);
+	//initialize_data_random(&data_h, data_size);
+	initialize_data_random_cudaMallocHost(&data_h, data_size);
 	initialize_data_zero(&output_h, data_size);
-	initialize_data_zero(&device_output_h, data_size);
+	//initialize_data_zero(&device_output_h, data_size);
+	initialize_data_zero_cudaMallocHost(&device_output_h, data_size);
 	
 	// Initialize data on Device
 	CUDA_CHECK_RETURN(cudaMalloc((void **)&data_d, sizeof(int)*data_size));
@@ -61,22 +65,31 @@ int main(int argc, char *argv[]){
 	// CUDA Parallel vector operation
 	
 	// TODO: compute grid_size
-	grid_size = (data_size - 1) / (block_size * work_per_thread) + 1;
-	dim3 grid_dime(grid_size, 1, 1);
-	dim3 block_dime(block_size, 1, 1);
 
 	set_clock();
 
-	CUDA_CHECK_RETURN(cudaMemcpy(data_d, data_h, sizeof(int)*data_size, cudaMemcpyHostToDevice));
 
-	vector_operation_kernel<<< grid_dime, block_dime >>>(output_d, data_d, data_size, work_per_thread);
-	
+	int stream_size = data_size / stream_count;
+	int stream_bytes = stream_size * sizeof(data_d[0]);
+
+	grid_size = (stream_size - 1) / block_size + 1;
+	dim3 grid_dime(grid_size, 1, 1);
+	dim3 block_dime(block_size, 1, 1);
+	int offset = 0;
+
+	for(int i = 0; i < stream_count; i++){
+		
+		cudaMemcpyAsync(&data_d[offset], &data_h[offset], stream_bytes, cudaMemcpyHostToDevice, streams[i]);
+		
+		vector_operation_kernel<<< grid_dime, block_dime, 0, streams[i]>>>(&output_d[offset], &data_d[offset], stream_size);
+
+		cudaMemcpyAsync(&device_output_h[offset], &output_d[offset], stream_bytes, cudaMemcpyDeviceToHost, streams[i]);
+		offset += stream_size;
+	}
+
 	CUDA_CHECK_RETURN(cudaDeviceSynchronize()); // Wait for the GPU launched work to complete
 	CUDA_CHECK_RETURN(cudaGetLastError());
 	
-	//Copy back the result
-	CUDA_CHECK_RETURN(cudaMemcpy(device_output_h, output_d, sizeof(int)*data_size, cudaMemcpyDeviceToHost));
-
     elapsed_time = get_elapsed_time();
 
     printf("-> CUDA vector operation time: %.4fms\n", elapsed_time / 1000);
@@ -84,10 +97,15 @@ int main(int argc, char *argv[]){
     #ifdef  TEST
     validate(output_h, device_output_h, data_size);
     #endif
-	
-	free(data_h);
+
+	for (int i = 0; i < stream_count; i++){
+        cudaStreamDestroy(streams[i]);
+    }
+	//free(data_h);
+	CUDA_CHECK_RETURN(cudaFreeHost(data_h));
 	free(output_h);
-	free(device_output_h);
+	//free(device_output_h);
+	CUDA_CHECK_RETURN(cudaFreeHost(device_output_h));
 
 	CUDA_CHECK_RETURN(cudaFree(output_d));
 	CUDA_CHECK_RETURN(cudaFree(data_d));
